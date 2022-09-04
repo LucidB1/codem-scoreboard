40b8680669f8fa18c166bf51ec5598ba79fda4e4
LucidB1 <82596430+LucidB1@users.noreply.github.com>
2022-09-04 20:58:32 +0300

    token

--- config.lua
+++ config.lua
@@ -3,9 +3,10 @@ Config = {}
 
 ------------------------------MAIN ---------
 Config.Framework = "newqb" -- newqb, oldqb, esx
-Config.BotToken = "ODg3MDcwNDkxOTA5Njg1MjU5.G5x5Y4.jgTx9K3UY7-mPmixtO4AG_Ymk3G6Dgj5bWJsQQ"
+Config.BotToken = "" -- Insert bot token if Config.Service set to "discord"
+Config.SteamApiKey = "" -- Insert steam api key if Config.Service set to "steam" 
+
 Config.OpenKey = 'e'
-Config.SteamApiKey = 'D9476AF846FD81484BC01250A72D6403'
 Config.MaxPlayer = '127'
 Config.Service = 'discord' -- steam , discord , none
 Config.ServerName = 'Codem RolePlay'
